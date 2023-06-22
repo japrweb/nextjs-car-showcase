ef7e02a98969b93e577f9b184c815e23eb4f998f
John A. Pérez R <japr893@gmail.com>
2023-06-21 20:32:23 -0400

    Setup searchBar and customFilters

---
+++ Components/SearchBar.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { SearchManufacturer } from "./";
+
+const SearchBar = () => {
+  const handleSearch = () => {};
+
+    return (
+    <form className='searchbar' onSubmit={handleSearch}>
+        <div className="searchbar__item">
+            <SearchManufacturer />
+        </div>
+    </form>
+  )
+}
+
+export default SearchBar
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
 import Image from "next/image";
 
-import { Hero } from "@/Components";
+import { CustomFilter, Hero, SearchBar } from "@/Components";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl  font-extrabold"> Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div
+            className="home_filter-container"
+          >
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
