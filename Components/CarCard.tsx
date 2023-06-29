01011a471809065a6c983ba1c530c2053bbc67aa
John A. Pérez R <japr893@gmail.com>
2023-06-29 18:14:56 -0400

    CustomButton & CarDetails structure

--- Components/CarCard.tsx
+++ Components/CarCard.tsx
@@ -5,6 +5,7 @@ import { CarProps } from "@/types";
 
 import CustomButton from './CustomButton';
 import { calculateCarRent } from "@/utils";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -12,6 +13,8 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const carRent = calculateCarRent(city_mpg, year);
 
@@ -59,8 +62,18 @@ const CarCard = ({ car }: CarCardProps) => {
           </div>
         </div>
 
-        
+        <div className="car-card__btn-container">
+          <CustomButton 
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/righ-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
       </div>
+
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
     </div>
   )
 }
--- Components/CustomButton.tsx
+++ Components/CustomButton.tsx
@@ -1,20 +1,37 @@
 "use client";
 
-import { CustomButtonProps } from '@/types';
+import Image from "next/image";
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+}: CustomButtonProps) => {
   return (
     <button
-        disabled={false}
-        type={btnType || 'button'}
-        className={`custom-btn ${containerStyles}`}
-        onClick={handleClick}
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
     >
-        <span className={`flex-1 `}>
-            {title}
-        </span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
