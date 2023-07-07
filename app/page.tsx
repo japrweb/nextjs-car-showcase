5cf0bf1c77cae194eb6d956fc0095e00f6726428
John A. Pérez R <japr893@gmail.com>
2023-07-07 13:24:28 -0400

    Filter configuration beginning

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,14 @@ import Image from "next/image";
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/Components";
 import { fetchCars } from "@/utils";
 
-export default async function Home() {
-  const allCars = await fetchCars();  
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2023,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });  
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {  
 		'X-RapidAPI-Key': '2b57e7d6cfmsh4143f4af5d9a501p1d7c37jsnb5cf21f61c5c',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit${limit}&fuel_type=${fuel}`, {
         headers: headers,
     });
 
